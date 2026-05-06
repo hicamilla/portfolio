2aeff47e54f5af73ec630582b56ddb00c36923dc
Camilla C <154556489+hicamilla@users.noreply.github.com>
2026-05-06 15:30:12 +0200

    add base styles, fonts and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,10 @@
+import './Navbar.css'
+
 function Navbar() {
   return (
-    <nav>
-      <a href="#home">Camilla Cardoso</a>
-      <ul>
+    <nav className="navbar">
+      <a href="#home" className="navbar-name">Camilla Cardoso</a>
+      <ul className="navbar-links">
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
